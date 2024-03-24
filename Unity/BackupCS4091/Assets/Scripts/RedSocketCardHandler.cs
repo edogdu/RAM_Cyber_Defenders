4530f2e97fb21f71492cd8906f9d810ff769729e
LightTheBlues <144945400+LightTheBlues@users.noreply.github.com>
2024-03-24 16:22:11 -0500

    restore

--- Unity/BackupCS4091/Assets/Scripts/RedSocketCardHandler.cs
+++ Unity/BackupCS4091/Assets/Scripts/RedSocketCardHandler.cs
@@ -1,19 +1,25 @@
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine;
+using System;
+using System.Collections;
 
-public class GreenSocketCardHandler : XRSocketInteractor
+public class RedSocketCardHandler : XRSocketInteractor
 {
-    [SerializeField] public GameObject attachedObject;
     private DeckManager deckManager;
     private bool isInSocket = false;
     [SerializeField] private bool isOccupied = false;
     [SerializeField] private int WhatSymbolSocket = 0;
     [SerializeField] protected bool doFilterByType = true;
-    [SerializeField] public bool isObjectAttached = false;
-
+    [SerializeField] private XRSocketCardHandler blueSocketHandler;
+    [SerializeField] private GreenSocketCardHandler greenSocketHandler;
+    [SerializeField] public GameObject attachedObject;
+    [SerializeField] private GameObject blueAttachObject;
+    [SerializeField] private GameObject greenAttachObject;
     protected override void Start()
     {
         deckManager = FindObjectOfType<DeckManager>();
+        greenSocketHandler = greenSocketHandler.GetComponent<GreenSocketCardHandler>();
+        blueSocketHandler = blueSocketHandler.GetComponent<XRSocketCardHandler>();
     }
 
     void Update()
@@ -25,22 +31,15 @@ public class GreenSocketCardHandler : XRSocketInteractor
         }
         else
         {
-            this.interactionLayers = InteractionLayerMask.GetMask("Green");
+            this.interactionLayers = InteractionLayerMask.GetMask("Red");
         }
-        if(attachedObject != null)
+        blueAttachObject = blueSocketHandler.GetAttachedObject();
+        if (greenSocketHandler.isObjectAttached == true)
         {
-            isObjectAttached = true;
-        }
-        else
-        {
-            isObjectAttached = false;
+            greenAttachObject = greenSocketHandler.GetAttachedObject();
         }
     }
-    public GameObject GetAttachedObject()
-    {
-        // Return the GameObject this script is attached to
-        return attachedObject;
-    }
+
     public bool IsOccupied()
     {
         return isOccupied;
@@ -80,15 +79,17 @@ public class GreenSocketCardHandler : XRSocketInteractor
             if (isOccupied == false)
             {
                 // Check if the card type matches the expected type\
-                
+
                 return cardInfo.GetSymbol() == WhatSymbolSocket;
             }
         }
         return base.CanSelect(interactable);
     }
+    [Obsolete]
     protected override void OnSelectEntered(XRBaseInteractable interactable)
     {
         attachedObject = interactable.gameObject;
+        StartCoroutine(WaitAndDestroy()); // Start the coroutine
         base.OnSelectEntered(interactable);
         if (interactable is XRGrabInteractable)
         {
@@ -97,16 +98,35 @@ public class GreenSocketCardHandler : XRSocketInteractor
             {
                 isOccupied = true; // Object is now in the socket
                 isInSocket = true;
+
                 interactable.selectExited.AddListener(OnSelectExited);
             }
         }
     }
-
+    [Obsolete]
     protected override void OnSelectExited(XRBaseInteractable interactable)
     {
         base.OnSelectExited(interactable);
         isOccupied = false; // Object has left the socket
         isInSocket = false;
         interactable.selectExited.RemoveListener(OnSelectExited);
+    }
+
+    IEnumerator WaitAndDestroy()
+    {
+        yield return new WaitForSeconds(4);
+        Debug.Log("Finished waiting for 4 seconds");
+        if(greenAttachObject != null)
+        {
+            Destroy(attachedObject);
+            Destroy(greenAttachObject);
+            
+        }
+        else
+        {
+            Destroy(attachedObject);
+            Destroy(blueAttachObject);
+            
+        }
     }
 }
